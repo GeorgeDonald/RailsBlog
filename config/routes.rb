d48f2787aba4392a47f7b42ebcfaf73991a8ef43
George Donald <dongsy326@gmail.com>
2017-11-26 22:46:48 -0500

    finished ruby code, working on css

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,8 @@ Rails.application.routes.draw do
   delete '/comments/:id', to: 'comments#delete'
   patch '/comments/:id', to: 'blogs#editcomment'
 
+  post '/blogs/next', to: 'blogs#nextpage'
+  post '/blogs/prev', to: 'blogs#prevpage'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
